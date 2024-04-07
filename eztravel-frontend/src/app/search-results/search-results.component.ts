9d361c2d0e486f8593e706a8f49abaee689e0354
MrV09 <vladcirlugea@gmail.com>
2024-04-07 10:19:21 +0300

    Edited pages

--- eztravel-frontend/src/app/search-results/search-results.component.ts
+++ eztravel-frontend/src/app/search-results/search-results.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss'
 })
