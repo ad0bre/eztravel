4d290dfed680433dcd85be6a329edaf0e2ec2c8f
MrV09 <vladcirlugea@gmail.com>
2024-04-08 16:56:03 +0300

    Changes to register

--- eztravel-frontend/src/app/add-transport/add-transport.component.ts
+++ eztravel-frontend/src/app/add-transport/add-transport.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-add-transport',
@@ -9,5 +8,5 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
   styleUrl: './add-transport.component.scss'
 })
 export class AddTransportComponent {
-  constructor(public modalRef: MdbModalRef<ModalCom)
+  
 }
