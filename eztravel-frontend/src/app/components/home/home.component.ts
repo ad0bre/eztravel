0fcd46419d25ada2e5f10c20b26f12e214455881
MrV09 <vladcirlugea@gmail.com>
2024-05-19 18:53:59 +0300

    Changed some aspect + deleted unused code

--- eztravel-frontend/src/app/components/home/home.component.ts
+++ eztravel-frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
